6635969b57f0e1d2a273fe4cb86f9da54dc8f878
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-16 19:37:48 +0900

    Update WritedReview.sql

--- CoupangEatsDB/WritedReview.sql
+++ CoupangEatsDB/WritedReview.sql
@@ -9,26 +9,26 @@ SELECT a.id AS 리뷰id
         , f.name AS 먹은음식
  FROM review a
  LEFT JOIN user b
-				    ON a.userId = b.id
+		ON a.userId = b.id
  LEFT JOIN ( SELECT id
-							      , name
-					     FROM restaurant ) AS c
-               ON a.restaurantId = c.id
+		    , name
+		FROM restaurant ) AS c
+               	ON a.restaurantId = c.id
  LEFT JOIN ( SELECT id
-							      , userId
-					     FROM cart
-               GROUP BY userId ) AS d
-               ON a.userId = d.id
+		   , userId
+	        FROM cart
+		GROUP BY userId ) AS d
+		ON a.userId = d.id
  LEFT JOIN ( SELECT id
-							      , cartId
+		    , cartId
                     , menuId
-					     FROM cartedmenu
-               GROUP BY cartId ) AS e
-               ON d.id = e.cartId
+		FROM cartedmenu
+		GROUP BY cartId ) AS e
+		ON d.id = e.cartId
  LEFT JOIN ( SELECT id
-							      , name
+		    , name
                     , restaurantId
-					     FROM menu
-               GROUP BY restaurantId ) AS f
-               ON c.id = f.restaurantId
+		FROM menu
+		GROUP BY restaurantId ) AS f
+		ON c.id = f.restaurantId
 WHERE a.userId = 1;
