36d8bd94c4754a27146023acdfa5293afd5c6f13
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-16 19:26:10 +0900

    Update RestaurantMain.sql

--- CoupangEatsDB/RestaurantMain.sql
+++ CoupangEatsDB/RestaurantMain.sql
@@ -1,6 +1,6 @@
 SELECT a.id AS 식당id
-		   , a.name AS 식당명 
-		   , starGrade AS 별점
+       , a.name AS 식당명 
+       , starGrade AS 별점
        , starCount AS 리뷰수
        , delTime AS 배달시간
        , delCost AS 배달비
@@ -15,25 +15,25 @@ SELECT a.id AS 식당id
        , c.cost AS 가격
        , c.contents AS 메뉴설명
   FROM restaurant a 
-  LEFT JOIN ( select id
-						   , restaurantId
-						   , ROUND(SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
+  LEFT JOIN ( SELECT id
+	       , restaurantId
+	       , ROUND(SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
                , COUNT(restaurantId) AS 'starCount'
                , imageUrl
                , contents
                , score
                , createdAt
-				          FROM review
-                  GROUP BY restaurantId ) AS b
-                  ON a.id = b.restaurantId
+		FROM review
+                GROUP BY restaurantId ) AS b
+                ON a.id = b.restaurantId
   LEFT JOIN ( SELECT id
-						   , restaurantId
-						   , name
-               , imageUrl
-               , contents
-               , cost
-					        FROM menu
-                  GROUP BY restaurantId) as c
-                  ON a.id = c.restaurantId
+		    , restaurantId
+		    , name
+               	    , imageUrl
+                    , contents
+                    , cost
+		FROM menu
+		GROUP BY restaurantId) AS c
+		ON a.id = c.restaurantId
  WHERE a.id = 1
  ORDER BY b.createdAt DESC;
