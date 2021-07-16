070edc50e5ec4b5eafa99559ba1b1b3924f3b32b
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-16 19:23:49 +0900

    Update NewRestaurant.sql

--- CoupangEatsDB/NewRestaurant.sql
+++ CoupangEatsDB/NewRestaurant.sql
@@ -1,7 +1,7 @@
 -- 새로 들어왔어요! 새로 추가된 식당 조회
 
 SELECT a.id
-		  , a.name AS 식당명
+	  , a.name AS 식당명
           , CASE WHEN a.createdAt > '2021-07-14 15:06:00' THEN '신규' END AS 상태
           , a.delCost AS 배달비
           , a.imageUrl AS 식당사진
