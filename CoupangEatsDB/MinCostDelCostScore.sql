ad9c81dc6f3e390e465efb3e842b1a2ad072931d
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-16 19:40:01 +0900

    Update MinCostDelCostScore.sql

--- CoupangEatsDB/MinCostDelCostScore.sql
+++ CoupangEatsDB/MinCostDelCostScore.sql
@@ -1,7 +1,7 @@
 -- 최소주문, 배달비, 별점 높은순 조회
 
 SELECT a.id AS 식당id
-			, a.name AS 식당명
+      , a.name AS 식당명
       , a.imageUrl AS 식당사진
       , starGrade AS 별점
       , starCount AS 리뷰수
@@ -10,11 +10,11 @@ SELECT a.id AS 식당id
       , CASE WHEN a.delTime <= '30' THEN '치타배달' ELSE '일반배달' END AS 배달유형
   FROM restaurant a
   LEFT JOIN ( SELECT id
-							       , restaurantId
+		     , restaurantId
                      , ROUND(SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
                      , COUNT(restaurantId) AS 'starCount'
-				        FROM review 
+		FROM review 
                 GROUP BY restaurantId ) AS b
                 ON a.id = b.restaurantId
-	WHERE a.minCost <= 12000 AND a.delCost <=3000
+  WHERE a.minCost <= 12000 AND a.delCost <=3000
   ORDER BY starGrade desc;
