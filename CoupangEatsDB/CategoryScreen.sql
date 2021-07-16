fbaaefc2e2f321f75ef1b5f8a8b375539f3205e8
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-16 19:21:53 +0900

    Update CategoryScreen.sql

--- CoupangEatsDB/CategoryScreen.sql
+++ CoupangEatsDB/CategoryScreen.sql
@@ -1,14 +1,14 @@
 -- 카테고리 선택 후 보이는 화면
 
 SELECT c.name AS 카테고리명
-			 ,a.id, a.name AS 식당명
+       ,a.id, a.name AS 식당명
        ,a.imageUrl AS 사진
        ,a.delCost AS 배달비
        ,starGrade AS 별점
        ,starCount AS 리뷰수
  FROM restaurant a
  LEFT JOIN ( SELECT id 
-						       ,ROUND ( SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
+		   ,ROUND ( SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
                    ,COUNT(restaurantId) AS 'starCount'
                    ,restaurantId
               FROM review
@@ -16,7 +16,7 @@ SELECT c.name AS 카테고리명
               ON a.id = b.restaurantId
               
   LEFT JOIN ( SELECT id, name, restaurantId
-			          FROM category
+		FROM category
                 GROUP BY id, name, restaurantId) AS c
                 ON a.id = c.restaurantId
 
