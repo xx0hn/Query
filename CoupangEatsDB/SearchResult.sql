63bc0c38b8e5a525ffc918405bc74c649b67ba0b
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-18 17:27:55 +0900

    Create SearchResult.sql

---
+++ CoupangEatsDB/SearchResult.sql
@@ -0,0 +1,30 @@
+ -- 검색어 검색 결과
+ 
+SELECT d.name AS 검색
+      , a.id AS 식당id
+      , a.name AS 식당명
+      , a.imageUrl AS 식당사진
+      , starGrade AS 별점
+      , starCount AS 리뷰수
+      , a.location AS 위치
+      , a.delCost AS 배달비
+      , a.delTime AS 배달시
+      , CASE WHEN a.status = 0 THEN '주문가능' ELSE '주문불가' END AS 상태 
+  FROM restaurant a
+  LEFT JOIN ( SELECT restaurantId
+                      , ROUND(SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
+                      , COUNT(restaurantId) AS 'starCount'
+                FROM review
+                GROUP BY restaurantId ) AS b
+                ON a.id = b.restaurantId
+  LEFT JOIN ( SELECT id
+                    , restaurantId
+                FROM category ) AS c
+                ON a.id = c.restaurantId
+  LEFT JOIN ( SELECT searchId
+                    , name
+                    , categoryId
+                FROM Search
+                GROUP BY categoryId) AS d
+                ON c.id = d.categoryId
+  WHERE d.categoryId = 13;
