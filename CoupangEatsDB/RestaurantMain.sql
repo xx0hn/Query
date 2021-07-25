2d36ddae2efd1f083d230d225bbcb96cb84f22fe
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-25 20:14:25 +0900

    Update RestaurantMain.sql

--- CoupangEatsDB/RestaurantMain.sql
+++ CoupangEatsDB/RestaurantMain.sql
@@ -3,12 +3,9 @@ SELECT a.id AS 식당id
        , starGrade AS 별점
        , starCount AS 리뷰수
        , delTime AS 배달시간
+       , a.delTime + 10 AS 최대배달시간
        , delCost AS 배달비
        , minCost AS 최소주문 
-       , b.id AS 리뷰id
-       , b.imageUrl AS 리뷰사진
-       , b.contents AS 리뷰내용
-       , b.score AS 리뷰점수
        , c.id AS 메뉴id
        , c.name AS 메뉴명
        , c.imageUrl AS 메뉴사진
@@ -19,10 +16,6 @@ SELECT a.id AS 식당id
 	       , restaurantId
 	       , ROUND(SUM(score) / COUNT(restaurantId), 1) AS 'starGrade'
                , COUNT(restaurantId) AS 'starCount'
-               , imageUrl
-               , contents
-               , score
-               , createdAt
 		FROM review
                 GROUP BY restaurantId ) AS b
                 ON a.id = b.restaurantId
@@ -35,4 +28,4 @@ SELECT a.id AS 식당id
 		FROM menu ) AS c
 		ON a.id = c.restaurantId
  WHERE a.id = 1
- ORDER BY b.createdAt DESC;
+ ORDER BY c.id ASC;
