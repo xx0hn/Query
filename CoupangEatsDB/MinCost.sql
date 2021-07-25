68f5ad8b9d2225f26300856560f433f4f0a2af19
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-25 20:37:07 +0900

    Update MinCost.sql

--- CoupangEatsDB/MinCost.sql
+++ CoupangEatsDB/MinCost.sql
@@ -3,10 +3,11 @@
 SELECT a.id AS 식당id
       , a.name AS 식당명
       , a.imageUrl AS 식당사진 
-      , starGrade AS 별점
-      , starCount AS 리뷰수
+      , CASE WHEN starGrade IS NULL THEN 0 ELSE starGrade END AS 별점
+      , CASE WHEN starCount IS NULL THEN 0 ELSE starCount END AS 리뷰수
       , a.delCost  AS 배달비
       , a.delTime AS 배달시간
+      , a.delTime AS 최대배달시간
       , CASE WHEN a.delTime <= '30' THEN '치타배달' ELSE '일반배달' END AS 배달유형
   FROM restaurant a
   LEFT JOIN ( SELECT id
