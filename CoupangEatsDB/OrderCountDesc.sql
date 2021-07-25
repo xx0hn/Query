1cdb902d8f3123d9cd87a4768e809c4e1982d84f
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-25 21:17:21 +0900

    Update OrderCountDesc.sql

--- CoupangEatsDB/OrderCountDesc.sql
+++ CoupangEatsDB/OrderCountDesc.sql
@@ -1,14 +1,15 @@
  -- 주문많은순 음식점 조회
  
- SELECT orderCount AS 주문횟수
+ SELECT CASE WHEN orderCount IS NULL THEN 0 ELSE orderCount END AS 주문횟수
         , a.name AS 식당명
         , a.imageUrl AS 식당사진
         , CASE WHEN a.delTime <= 30 THEN'치타배달' ELSE '일반배달' END AS 배달유형
-        , starGrade AS 별점
-        , starCount AS 리뷰수
+        , CASE WHEN starGrade IS NULL THEN 0 ELSE starGrade END AS 별점
+        , CASE WHEN starCount IS NULL THEN 0 ELSE starCount END AS 리뷰수
         , a.location AS 위치
         , a.delCost AS 배달비
         , a.delTime AS 배달시간
+	, a.delTime + 10 AS 최대배달시간
   FROM restaurant a
   LEFT JOIN ( SELECT id
                     , restaurantId
